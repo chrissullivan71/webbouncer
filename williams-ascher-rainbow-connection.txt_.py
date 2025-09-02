ceaf43ece7fdeb769092e96dbb807ed9b819c7ab
1eyedogbreath <132585185+1eyedogbreath@users.noreply.github.com>
2025-09-02 17:09:43 -0400

    Create williams-ascher-rainbow-connection.txt_.py
    rainbow connection lyrics

---
+++ williams-ascher-rainbow-connection.txt_.py
@@ -0,0 +1,115 @@
+The Rainbow Connection  Words and Music by Paul Williams
+3/4
+
+[Verse 1]
+C             Am      Dm          G7        
+Why are there so many songs about rainbows,
+1             4       7           10            ZZZ               LL 01	
+[Verse 1]
+C                 Am    Dm    G7
+And what's on the other side?
+    1             4     7     10                 ZZZ              LL 02	
+[Verse 1]	
+C            Am           Dm     G7           
+Rainbows are visions, but only illusions,
+1            4            7      10                     ZZZ       LL 03			
+[Verse 1]
+    C             Am         Dm    G7
+And rainbows have nothing to hide.       
+    1             4          7     10                 ZZZ         LL 04
+[Verse 1]	
+Fmaj7                                             
+So we've been told and some choose to believe it, 
+1             4             7           10               ZZZ      LL 05
+[Verse 1]          
+Em           
+I know they're wrong; wait and see.
+1              4               7                          ZZZ     LL 06
+[Chorus]
+Dm             G7           Em         A7       
+Some day we'll find it, the rainbow connection, 
+1              4            7          10                 ZZZ     LL 07
+[Chorus]
+    Dm          G7            C
+The lovers, the dreamers, and me.
+    1           4             7                           ZZZ     LL 08
+[Verse 2]
+C             Am              Dm           G7        
+Who said that every wish will be heard and answered,
+1             4               7            10             ZZZ     LL 09
+[Verse 2]
+     C             Am      Dm     G7
+When wished on the morning star?
+     1             4       7      10                         ZZZ  LL 10
+[Verse 2]
+C        Am                  Dm        G7         
+Somebody thought of that and someone believed it, 
+1        4                   7         10                   ZZZ   LL 11
+[Verse 2]
+    C              Am      Dm     G7
+And look what it's done so far.
+    1              4       7      10                        ZZZ   LL 12
+[Verse 2]
+Fmaj7                                       
+What's so amazing that keeps us stargazing, 
+1          4           7            10                      ZZZ   LL 13
+[Verse 2]
+    Em 
+and what do we think we might see?
+    1          4              7                             ZZZ   LL 14
+[Chorus]
+Dm             G7           Em        A7        
+Some day we'll find it, the rainbow connection, 
+1              4            7          10                  ZZZ    LL 15
+[Chorus]
+    Dm          G7           C
+The lovers, the dreamers and me.
+    1           4            7                             ZZZ    LL 16
+[Bridge]
+Em        Dm        C         Fmaj7          
+All of us under its spell, we know that it's 
+1         4         7         10                           ZZZ    LL 17
+[Bridge]
+C        Dm   G7
+probably ma - gic.
+1        4    7                                            ZZZ    LL 18
+[Verse 3]
+C             Am              Dm             G7      
+Have you been half asleep and have you heard voices, 
+1             4               7              10              ZZZ  LL 19
+[Verse 3]
+C               Am         Dm
+I've heard them calling my name.
+1               4          7       10                        ZZZ  LL 20
+[Verse 3]
+C           Am               Dm              G7       
+Is this the sweet sound that calls the young sailors? 
+1           4                7               10              ZZZ  LL 21
+[Verse 3]
+    C              Am          Dm     G7
+The voice might be one and the same.
+    1              4           7      10                      ZZZ LL 22
+[Verse 3]	
+Fmaj7                                    
+I've heard it too many times to ignore it
+1             4        7          10                   ZZZ        LL 23
+[Verse 3]
+     Em
+It's something that I'm supposed to be.
+     1              4               7                  ZZZ        LL 24	 
+[Chorus]
+Dm            G7          Em         A7       
+Someday we'll find it the rainbow connection,
+1             4           7          10                  ZZZ      LL 25       
+[Verse 3]
+    Dm          G7           C
+The lovers, the dreamers and me.
+    1           4            7                           ZZZ      LL 26
+[Bridge, end]
+Em        Dm         C      Dm       
+Da dee da dee dum de da, da da dee da
+1         4          7      10                           ZZZ      LL 27
+[Bridge, end]
+G7      C
+dum dee dee
+1       4                                                ZZZ      LL 28
